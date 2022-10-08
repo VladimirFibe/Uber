e73365e6660b6f02fd8eacae6b17d5d173e6e683
Vladimir <motiw@icloud.com>
2022-10-09 01:07:34 +0600

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Vladimir Fibe on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
